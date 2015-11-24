e1344feda2ee74bf9fcdbb3ac4beb6bf3e9bbddb
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-24 16:26:01 -0700

    :new: Add main module

---
+++ src/main.js
@@ -0,0 +1,22 @@
+'use babel'
+
+import Path from 'path'
+import Less from 'less'
+
+export const minifier = false
+export const compiler = true
+export function process(contents, {relativePath, absolutePath}, {state, config}) {
+  return Less.render(contents, Object.assign({}, config.less, {
+    filename: relativePath,
+    sourceMap: true,
+    paths: [Path.dirname(absolutePath)]
+  })).then(function(output) {
+    if (output.imports.length) {
+      state.imports = state.imports.concat(output.imports)
+    }
+    if (output.map) {
+      state.sourceMap = output.map.toString()
+    }
+    return output.css
+  })
+}
