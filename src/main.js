c1ba7d38d487b1b4addc0fa1ec3673c08daf3781
steelbrain <de.steelbrain@gmail.com>
2016-02-20 22:25:14 +0000

    :new: Bundle sources in map

--- src/main.js
+++ src/main.js
@@ -1,26 +1,38 @@
 'use babel'
 
+import FS from 'fs'
 import Path from 'path'
 import Less from 'less'
+import promisify from 'sb-promisify'
+
+const readFile = promisify(FS.readFile)
 
 export const minifier = false
 export const compiler = true
-export function process(contents, {rootDirectory, filePath, config, state}) {
-  return Less.render(contents, Object.assign({}, config.less, {
+export async function process(contents, {rootDirectory, filePath, config, state}) {
+  const output = await Less.render(contents, Object.assign({}, config.less, {
     filename: Path.relative(rootDirectory, filePath),
     sourceMap: true,
     paths: [Path.dirname(filePath)]
-  })).then(function(output) {
-    if (!output.map) {
-      return null
-    }
+  }))
 
-    output.imports.forEach(function(item) {
-      state.imports.push(Path.join(rootDirectory, item))
-    })
-    return {
-      contents: output.css,
-      sourceMap: JSON.parse(output.map)
-    }
+  if (!output.map) {
+    return null
+  }
+
+  output.imports.forEach(function(item) {
+    state.imports.push(Path.join(rootDirectory, item))
   })
+
+  const map = JSON.parse(output.map)
+  map.sourcesContent = []
+  await Promise.all(map.sources.map(async function(filePath) {
+    const fullPath = Path.join(rootDirectory, filePath)
+    map.sourcesContent.push((await readFile(fullPath)).toString())
+  }))
+
+  return {
+    contents: output.css,
+    sourceMap: map
+  }
 }
